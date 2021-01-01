a56b2436594683ee8190457362f6297b54f0cb5a
sunilchityala <sunilatwork24@gmail.com>
2021-01-01 11:35:23 +0530

    Adding @Post Controller Example

--- src/main/java/io/sunilbranch/springgithub/controller/GreetingController.java
+++ src/main/java/io/sunilbranch/springgithub/controller/GreetingController.java
@@ -2,6 +2,7 @@ package io.sunilbranch.springgithub.controller;
 
 import io.sunilbranch.springgithub.model.GreetingMessage;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,10 @@ public class GreetingController {
     @GetMapping("/greeting")
     public GreetingMessage greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new GreetingMessage(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @PostMapping("/createGreeting")
+    public String createGreeting(){
+        return "New Greetings created successfully";
     }
 }
